0802b98a81f0c3852db1ddcf7416b8a61b1f703d
Justin <jbbenson12@gmail.com>
2019-07-10 08:39:42 -1000

    pixel

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -17,6 +17,31 @@ function draw() {
   clearButton.innerHTML = "clear";
   buttons.appendChild(clearButton);
 
+  let biggerButton = document.createElement("button");
+  biggerButton.id = "bigger";
+  biggerButton.innerHTML = "size";
+  buttons.appendChild(biggerButton);
+
+  let undoButton = document.createElement("button");
+  undoButton.id = "undo";
+  undoButton.innerHTML = "undo";
+  buttons.appendChild(undoButton);
+
+  let redoButton = document.createElement("button");
+  redoButton.id = "redo";
+  redoButton.innerHTML = "redo";
+  buttons.appendChild(redoButton);
+
+  let saveButton = document.createElement("button");
+  saveButton.id = "save";
+  saveButton.innerHTML = "save";
+  buttons.appendChild(saveButton);
+
+  let loadButton = document.createElement("button");
+  loadButton.id = "load";
+  loadButton.innerHTML = "load";
+  buttons.appendChild(loadButton);
+
   let canvasWrapper = document.createElement("div");
   canvasWrapper.id = "canvas";
   painter.appendChild(canvasWrapper);
@@ -31,15 +56,18 @@ function draw() {
   let tblBody = document.createElement("tbody");
 
   // creating all cells
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 200; i++) {
     // creates a table row
     let row = document.createElement("tr");
+    row.className = "canvasTr";
 
     for (let j = 0; j < 200; j++) {
       // Create a <td> element and put the <td> at
       // the end of the table row
       let cell = document.createElement("td");
       cell.className = "canvasCells";
+      cell.id = j + 1 + ", " + (i + 1);
+      cell.style.backgroundColor = "ffffff";
       row.appendChild(cell);
     }
 
@@ -51,6 +79,11 @@ function draw() {
   tbl.appendChild(tblBody);
   // appends <table> into <body>
   canvas.appendChild(tbl);
+
+  let xyCoords = document.createElement("div");
+  xyCoords.id = "xyCoords";
+  //xyCoords.innerHTML = canvasCells.innerHTML;
+  canvas.appendChild(xyCoords);
 
   var colors = [];
   while (colors.length < 50) {
@@ -88,6 +121,20 @@ function draw() {
   // appends <table> into <body>
   palette.appendChild(tbl2);
 
+  let selectedColorText = document.createElement("p");
+  selectedColorText.id = "selectedColorText";
+  selectedColorText.innerHTML = "selected";
+  paletteWrapper.appendChild(selectedColorText);
+
+  let selectedColorText2 = document.createElement("p");
+  selectedColorText2.id = "selectedColorText2";
+  selectedColorText2.innerHTML = "color";
+  paletteWrapper.appendChild(selectedColorText2);
+
+  let selectedColor = document.createElement("div");
+  selectedColor.id = "selectedColor";
+  paletteWrapper.appendChild(selectedColor);
+
   let mouseDown = 0;
 
   document.body.onmouseup = function() {
@@ -100,8 +147,11 @@ function draw() {
   colorPicked.forEach(cell => {
     cell.addEventListener("click", () => {
       colorStored = cell.style.backgroundColor;
+      selectedColor.style.backgroundColor = colorStored;
     });
   });
+
+  let canvasCells = document.querySelectorAll(".canvasCells");
 
   let erasePicked = document.querySelector("#erase");
   erasePicked.addEventListener("click", fillCell);
@@ -111,26 +161,23 @@ function draw() {
 
   let clearPicked = document.querySelector("#clear");
   clearPicked.addEventListener("click", fillCells);
-
   function fillCells() {
-    let canvasCells = document.getElementsByClassName("canvasCells");
     for (let i = 0; i < canvasCells.length; i++) {
       canvasCells[i].style.backgroundColor = "ffffff";
     }
   }
 
-  let clickPix = document.querySelectorAll("td");
-  for (let i = 0; i < clickPix.length; i++) {
-    clickPix[i].addEventListener("mousedown", function() {
+  for (let i = 0; i < canvasCells.length; i++) {
+    canvasCells[i].addEventListener("mousedown", function() {
       mouseDown = 1;
     });
 
-    clickPix[i].addEventListener("click", fillCell);
-    function fillCell(e) {
-      this.style.backgroundColor = colorStored;
+    canvasCells[i].addEventListener("click", fillCell);
+    function fillCell() {
+      canvasCells[i].style.backgroundColor = colorStored;
     }
 
-    clickPix[i].addEventListener("mouseover", fillCellDrag);
+    canvasCells[i].addEventListener("mouseover", fillCellDrag);
     function fillCellDrag() {
       if (mouseDown === 1) {
         this.style.backgroundColor = colorStored;
